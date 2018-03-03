0506e55eac33b03ac5b5bdf7f297bdfb48539c76
salaris1 <30813640+salaris1@users.noreply.github.com>
2018-03-03 11:24:10 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='kernelpls',
       version='0.1',
       description='an implementation of kernel pls',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/salaris1/kernelpls.git',
       author='Sam Sals',
       author_email='sam@ssalari.net',
       license='MIT',
